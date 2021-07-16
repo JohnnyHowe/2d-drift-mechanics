7b48efffa7f4a26b4879bf5a3c3d66692a87e328
Johnny Howe <jonathon.o.howe@gmail.com>
2021-07-16 13:20:14 +1200

    Camera follow car

---
+++ Assets/Car/FollowCar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCar : MonoBehaviour
+{
+    public Transform car;
+    public float speed = 0.5f;
+    public float forwardOffset = 4f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 current = transform.position;
+        Vector3 next = car.transform.position + car.transform.up * forwardOffset;
+        next.z = current.z;
+        transform.position = Vector3.Lerp(current, next, speed * Time.fixedDeltaTime);
+    }
+}

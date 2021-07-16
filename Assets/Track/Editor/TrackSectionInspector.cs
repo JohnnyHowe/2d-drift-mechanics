59987b4cd79ed05989d47a45b9af56de66980570
Johnny Howe <jonathon.o.howe@gmail.com>
2021-07-16 12:12:13 +1200

    Track point editor p1

---
+++ Assets/Track/Editor/TrackSectionInspector.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TrackSection))]
+public class TrackSectionInspector : Editor
+{
+    private TrackSection track;
+    private Transform handleTransform;
+    private Quaternion handleRotation;
+
+    private void OnSceneGUI()
+    {
+        track = target as TrackSection;
+        handleTransform = track.transform;
+        handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+            handleTransform.rotation : Quaternion.identity;
+        
+        // Draw curves
+        int tl = track.curves.Length;
+        for (int i = 0; i < tl; i ++) {
+            DrawCurve(ref track.curves[i]);
+        }
+        track.curves[tl - 1].p3 = handleTransform.InverseTransformPoint(DrawPoint(handleTransform.TransformPoint(track.curves[tl - 1].p3), "Track Point"));
+        track.Validate();
+    }
+
+    private void DrawCurve(ref Curve curve) {
+        Vector3 start = handleTransform.TransformPoint(curve.p0);
+        Vector3 end = handleTransform.TransformPoint(curve.p3);
+
+        curve.p0 = handleTransform.InverseTransformPoint(DrawPoint(start, "Track Point"));
+        Handles.DrawLine(start, end);
+    }
+
+    private Vector3 DrawPoint(Vector3 point, string label) {
+        EditorGUI.BeginChangeCheck();
+        Vector3 p = Handles.DoPositionHandle(point, handleRotation);
+        Handles.Label(p, label);
+        // if (EditorGUI.EndChangeCheck())
+        // {
+        //     p = handleTransform.InverseTransformPoint(p);
+        // }
+        return p;
+    }
+}
--- Assets/Track/TrackSection.cs
+++ Assets/Track/TrackSection.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TrackSection : MonoBehaviour
 {
+
+    public Curve[] curves;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +19,23 @@ public class TrackSection : MonoBehaviour
     {
         
     }
+
+    public void Validate() {
+        OnValidate();
+    }
+
+    void OnValidate() {
+        for (int i = 0; i < curves.Length - 1; i++) {
+            curves[i].p3 = curves[i + 1].p0;
+        }
+    }
+}
+
+
+[Serializable]
+public struct Curve {
+    public Vector3 p0;
+    public Vector3 p1;
+    public Vector3 p2;
+    public Vector3 p3;
 }

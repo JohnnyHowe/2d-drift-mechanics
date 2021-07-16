f73fe0c7bec61287844bcc30ca1330ec7c75825d
Johnny Howe <jonathon.o.howe@gmail.com>
2021-07-16 15:33:19 +1200

    a

--- Assets/Car/FollowCar.cs
+++ Assets/Car/FollowCar.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class FollowCar : MonoBehaviour
 {
     public Transform car;
-    public float speed = 0.5f;
+    public float speed = 1f;
+    public float rotSpeed = 1f;
     public float forwardOffset = 4f;
 
     // Update is called once per frame
@@ -16,5 +17,10 @@ public class FollowCar : MonoBehaviour
         float distance = (current - next).magnitude;
         next.z = current.z;
         transform.position = Vector3.Lerp(current, next, speed * Time.fixedDeltaTime * distance * distance);
+
+        Quaternion currentRot = transform.rotation;
+        Quaternion nextRot = car.transform.rotation;
+        float distanceRot = Quaternion.Angle(currentRot, nextRot);
+        transform.rotation = Quaternion.Lerp(currentRot, nextRot, rotSpeed * Time.fixedDeltaTime * distanceRot * distanceRot);
     }
 }
--- Assets/Car/PlayerController.cs
+++ Assets/Car/PlayerController.cs
@@ -7,13 +7,16 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        controller.throttle = 0;
-        if (Input.GetKey(KeyCode.W)) controller.throttle += 1;
-        if (Input.GetKey(KeyCode.S)) controller.throttle -= 1;
-        controller.steering = 0;
-        if (Input.GetKey(KeyCode.D)) controller.steering += 1;
-        if (Input.GetKey(KeyCode.A)) controller.steering -= 1;
+        // controller.throttle = 0;
+        // if (Input.GetKey(KeyCode.W)) controller.throttle += 1;
+        // if (Input.GetKey(KeyCode.S)) controller.throttle -= 1;
+        // controller.steering = 0;
+        // if (Input.GetKey(KeyCode.D)) controller.steering += 1;
+        // if (Input.GetKey(KeyCode.A)) controller.steering -= 1;
         // trail.SetActive(controller.IsDrift());
         trail.emitting = controller.ShouldDrift();
+
+        controller.throttle = 1;
+        controller.steering = -TouchInput.centeredScreenPosition.x;
     }
 }

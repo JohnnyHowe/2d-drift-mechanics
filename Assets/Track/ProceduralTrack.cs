62887cc6c245446fb19874b8276b5a621df1b77d
Johnny Howe <jonathon.o.howe@gmail.com>
2021-08-04 19:59:19 +1200

    Procedural track generator is back baby

--- Assets/Track/ProceduralTrack.cs
+++ Assets/Track/ProceduralTrack.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProceduralTrack : MonoBehaviour
+public class ProceduralTrack : Track
 {
     [Header("Start of track")]
     public Vector2 startPosition = Vector2.zero;
@@ -13,10 +13,14 @@ public class ProceduralTrack : MonoBehaviour
     [Header("Procedural generation")]
     public TrackSection[] trackSections;
     public LineRenderer lineRenderer;
+    public Transform car;
+    public int forwardBufferSections = 5;
+    public int rearBufferSections = 3;
     [Header("Other")]
     public int splineAccuracy = 25;
     public float scale = 10;
     private List<TrackSection> currentTrackSections;
+    public int g;
 
 
     void Start()
@@ -24,6 +28,49 @@ public class ProceduralTrack : MonoBehaviour
         currentTrackSections = new List<TrackSection>();
         CreateStart();
         UpdateRenderer();
+    }
+
+    void FixedUpdate() {
+        if (CreateSections()) {
+            UpdateRenderer();
+        }
+    }
+
+    /// <summary>
+    /// If required by car position, create and delete sections
+    /// returns whether change was made
+    /// </summary>
+    bool CreateSections() {
+        int currentIndex = GetCarSectionIndex();
+        int finalIndex = currentIndex + forwardBufferSections;
+        int numGenerate = Mathf.Max(0, finalIndex - currentTrackSections.Count);
+        bool change = false;
+        for (int i = 0; i < numGenerate; i ++) {
+            change = true;
+            CreateRandomSection();
+        }
+        while (currentTrackSections.Count > forwardBufferSections + rearBufferSections) {
+            currentTrackSections.RemoveAt(0);
+        }
+        return change;
+    }
+
+    /// <summary>
+    /// What section is the car on (or closest to?)
+    /// returned as index of currrentTrackSections
+    /// </summary>
+    int GetCarSectionIndex() {
+        int closestIndex = 0;
+        float closestDistance = -1;
+        for (int i = 0; i < currentTrackSections.Count; i++) {
+            Vector2 lastPoint = currentTrackSections[i].curvePoints[currentTrackSections[i].curvePoints.Length - 1];
+            float distance = ((Vector2) car.position - lastPoint).magnitude;
+            if (distance < closestDistance || closestDistance == -1) {
+                closestDistance = distance;
+                closestIndex = i;
+            }
+        }
+        return closestIndex;
     }
 
     void CreateStart() {
@@ -34,6 +81,10 @@ public class ProceduralTrack : MonoBehaviour
         foreach (TrackSection section in startTrackSections) {
             AddNewSection(section);
         }
+    }
+
+    void CreateRandomSection() {
+        AddNewSection(trackSections[Random.Range(0, trackSections.Length)]);
     }
 
     void AddNewSection(TrackSection section) {
@@ -49,7 +100,7 @@ public class ProceduralTrack : MonoBehaviour
     void AlignSection(TrackSection instantiatedSection, Vector2 startPosition, Vector2 startDirection) {
         Vector2 offset = startPosition - instantiatedSection.curvePoints[0];
         Vector2 startGradient = (instantiatedSection.curvePoints[0] - instantiatedSection.curvePoints[1]).normalized;
-
+        // TODO what if curve 1 and 2 are same?
         for (int i = 0; i < instantiatedSection.curvePoints.Length; i++) {
             Vector2 transformedPoint = instantiatedSection.curvePoints[i] + offset;
             instantiatedSection.curvePoints[i] = RotateAround(transformedPoint, startPosition, 180-Vector2.SignedAngle(startGradient, lastEndDirection));
@@ -61,7 +112,7 @@ public class ProceduralTrack : MonoBehaviour
 
         newSection.curvePoints = new Vector2[section.curvePoints.Length];
         for (int i = 0; i < section.curvePoints.Length; i++) {
-            newSection.curvePoints[i] = section.curvePoints[i];
+            newSection.curvePoints[i] = section.curvePoints[i] * scale;
         }
         return newSection;
     }
@@ -94,7 +145,7 @@ public class ProceduralTrack : MonoBehaviour
                 {
                     float t = (float)i / splineAccuracy;
                     Vector2 current = BezierCurve.GetPoint(section.curvePoints[0 + offset], section.curvePoints[1 + offset], section.curvePoints[2 + offset], section.curvePoints[3 + offset], t);
-                    points.Add(current * scale);
+                    points.Add(current);
                 }
             }
         }
@@ -102,9 +153,9 @@ public class ProceduralTrack : MonoBehaviour
         lineRenderer.SetPositions(points.ToArray());
     }
 
-    // Update is called once per frame
-    void Update()
+    public override bool IsOnTrack(Vector2 point)
     {
-
+        // TODO
+        throw new System.NotImplementedException();
     }
 }
